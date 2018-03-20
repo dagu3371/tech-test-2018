347322fb8931f4e9f37354844dcf3b593dff397b
David Gu <dagu3371@gmail.com>
2018-03-20 17:18:19 +0000

    Move all voucher calculation logic into the voucher class

--- lib/order.rb
+++ lib/order.rb
@@ -12,61 +12,13 @@ class Order
     voucher_type = @voucher.nil? ? nil : @voucher.type
     case voucher_type
     when :discount
-      calculate_discount
+      @voucher.calculate_discount
     when :credit
-      calculate_credit
+      @voucher.calculate_credit
     when :loyalty
-      calculate_loyalty
+      @voucher.calculate_loyalty
     else
       return DEFAULT_PRICE
     end
-  end
-
-  def calculate_loyalty
-    if @voucher.loyalty_points == @voucher.number
-      @voucher.loyalty_points = 0
-      return 0.0
-    else
-      @voucher.loyalty_points += 1
-      return DEFAULT_PRICE
-    end
-  end
-
-  def calculate_credit
-    @voucher.amount = @voucher.amount - DEFAULT_PRICE
-    cost = @voucher.amount
-    if cost < 0
-      @voucher.amount = 0.0
-      return cost.abs
-    else
-      return 0.0
-    end
-  end
-
-  def calculate_discount
-    if @voucher.number > 0
-      discount = @voucher.instant == true ? instant_pay : pay_as_you_go
-      @voucher.number -= 1
-      return discount
-    else
-      return DEFAULT_PRICE
-    end
-  end
-
-  private
-
-  def instant_pay
-    bulk_cost = discounted_price * @voucher.number
-    @voucher.amount = DEFAULT_PRICE * (@voucher.number - 1)
-    @voucher.type = :credit
-    return bulk_cost
-  end
-
-  def pay_as_you_go
-    discounted_price
-  end
-
-  def discounted_price
-    DEFAULT_PRICE * (@voucher.amount.to_f/100)
   end
 end
--- lib/voucher.rb
+++ lib/voucher.rb
@@ -1,4 +1,5 @@
 class Voucher
+  DEFAULT_PRICE = 6.95
   attr_accessor :type, :amount, :number, :instant, :loyalty_points
 
   def initialize(type, **attrs)
@@ -11,5 +12,51 @@ class Voucher
 
   def self.create(type, **attrs)
     new(type, **attrs)
+  end
+
+  def calculate_loyalty
+    if @loyalty_points == @number
+      @loyalty_points = 0
+      return 0.0
+    else
+      @loyalty_points += 1
+      return DEFAULT_PRICE
+    end
+  end
+
+  def calculate_credit
+    @amount -= DEFAULT_PRICE
+    if @amount < 0.0
+      return @amount.abs
+    else
+      return 0.0
+    end
+  end
+
+  def calculate_discount
+    if @number > 0
+      discount = @instant == true ? instant_pay : pay_as_you_go
+      @number -= 1
+      return discount
+    else
+      return DEFAULT_PRICE
+    end
+  end
+
+  private
+
+  def instant_pay
+    bulk_cost = discounted_price * @number
+    @amount = DEFAULT_PRICE * (@number - 1)
+    @type = :credit
+    return bulk_cost
+  end
+
+  def pay_as_you_go
+    discounted_price
+  end
+
+  def discounted_price
+    DEFAULT_PRICE * (@amount.to_f/100)
   end
 end
